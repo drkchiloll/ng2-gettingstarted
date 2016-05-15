727ede84559a1dc803c2af4056e1f27be882bb1d
drkchiloll <samwomack_@me.com>
2016-05-15 15:25:12 -0500

    Created Interface for Product Properties in order to ensure the ProductListComp products are safe to use (strong type to ensure data safety)

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'pm-products',
@@ -11,7 +12,7 @@ export class ProductListComponent {
   imageMargin: number = 2;
   showImage: boolean = false;
   listFilter: string = 'cart';
-  products: any[] = [{
+  products: Product[] = [{
     "productId": 1,
     "productName": "Leaf Rake",
     "productCode": "GDN-0011",
