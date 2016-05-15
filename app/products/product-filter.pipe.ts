0ea17efbe08c7729ad04ad6a4864b18b3d085bbc
drkchiloll <samwomack_@me.com>
2016-05-15 17:55:57 -0500

    Added Custom Data PIPE which filters the list of Products based on the input value in the HTML.

---
+++ app/products/product-filter.pipe.ts
@@ -0,0 +1,15 @@
+import { PipeTransform, Pipe } from '@angular/core';
+import { Product } from './product';
+
+@Pipe({
+  name: 'productFilter'
+})
+
+export class ProductFilterPipe implements PipeTransform {
+  transform(value: Product[], args: string): Product[] {
+    let filter: string = args[0] ? args[0].toLocaleLowerCase() + args.substring(1): null;
+    console.log(filter);
+    return filter ? value.filter((product: Product) =>
+      product.productName.toLocaleLowerCase().indexOf(filter) != -1) : value;
+  }
+}
