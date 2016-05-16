d1289856c1f98014fc797545da7245b883ad3cd5
drkchiloll <samwomack_@me.com>
2016-05-16 13:56:08 -0500

    Added NG2 Router is all necessary to make work

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,24 +2,49 @@ import { Component } from '@angular/core';
 // HTTP Services
 import { HTTP_PROVIDERS } from '@angular/http';
 import 'rxjs/Rx';
+import { ROUTER_PROVIDERS, Routes, ROUTER_DIRECTIVES } from '@angular/router';
 
 import { ProductListComponent } from './products/product-list.component';
 import { ProductService } from './products/product.service';
+import { WelcomeComponent } from './home/welcome.component';
 
 @Component({
   selector: 'pm-app',
   template: (
     `
       <div>
-        <h1>{{pageTitle}}</h1>
-        <pm-products></pm-products>
+        <nav class='navbar navbar-default'>
+          <div class='container-fluid'>
+            <a class='navbar-brand'>{{pageTitle}}</a>
+            <ul class='nav navbar-nav'>
+              <li><a [routerLink]="['/welcome']">Home</a></li>
+              <li><a [routerLink]="['/products']">Product List</a></li>
+            </ul>
+          </div>
+        </nav>
+        <div class='container'>
+          <router-outlet></router-outlet>
+        </div>
       </div>
     `
   ),
-  directives: [ProductListComponent],
+  directives: [ProductListComponent,
+               ROUTER_DIRECTIVES],
   providers: [ProductService,
-              HTTP_PROVIDERS]
+              HTTP_PROVIDERS,
+              ROUTER_PROVIDERS]
 })
+
+@Routes([{
+  path: '/',
+  component: WelcomeComponent
+},{
+  path: '/welcome',
+  component: WelcomeComponent
+}, {
+  path: '/products',
+  component: ProductListComponent
+}])
 
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
