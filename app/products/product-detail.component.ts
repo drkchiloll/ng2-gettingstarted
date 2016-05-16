702d8992acdf838963319a638a62b2da971a27c2
drkchiloll <samwomack_@me.com>
2016-05-16 15:20:57 -0500

    New Component

---
+++ app/products/product-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: `app/products/product-details.html`
+})
+
+export class ProductDetailComponent {
+  pageTitle: string = 'Product Detail';
+}
