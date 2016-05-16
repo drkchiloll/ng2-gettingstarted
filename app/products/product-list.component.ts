00989f297f6d59e82e83108bfbd2c3ee5de0c3d9
drkchiloll <samwomack_@me.com>
2016-05-16 11:53:47 -0500

    Retrieve the appropriate products from the Service; HttpReq's use the .subscribe method for data retreival

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -18,6 +18,7 @@ export class ProductListComponent implements OnInit {
   imageMargin: number = 2;
   showImage: boolean = false;
   listFilter: string;
+  errorMessage: string;
   products: Product[];
 
   constructor(private _productService: ProductService) {}
@@ -27,7 +28,12 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this._productService.getProducts();
+    this._productService.getProducts()
+      .subscribe(
+        products => this.products = products,
+        error => this.errorMessage = <any>error
+      );
+
   }
 
   onRatingClicked(message: string): void {
