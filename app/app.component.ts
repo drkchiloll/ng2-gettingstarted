91a2a829ccf96947bec855f6fe6429dc2ba34b6c
drkchiloll <samwomack_@me.com>
2016-05-16 11:49:23 -0500

    Import @angular/http HTTP_PROVIDERS and rxjs (Observables) Add Providers to the @Component providers list

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+// HTTP Services
+import { HTTP_PROVIDERS } from '@angular/http';
+import 'rxjs/Rx';
+
 import { ProductListComponent } from './products/product-list.component';
 import { ProductService } from './products/product.service';
 
@@ -13,7 +17,8 @@ import { ProductService } from './products/product.service';
     `
   ),
   directives: [ProductListComponent],
-  providers: [ProductService]
+  providers: [ProductService,
+              HTTP_PROVIDERS]
 })
 
 export class AppComponent {
